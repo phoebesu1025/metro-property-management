dcb4c6e7701e648ff8e2275ef32bf1d75a197212
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-21 11:42:03 +1300

    added Components folder, and tailwind setup

---
+++ frontend/src/Components/JustTest.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const JustTest = () => {
+    return (
+        <>
+            <div className='text-center text-green-300 text-7xl'>TEST TAILWIND</div>
+            <div className='text-center text-pink-300 text-7xl'>TEST TAILWIND</div>
+            <div className='text-center text-blue-300 text-7xl'>TEST TAILWIND</div>
+            <div className='text-center text-purple-300 text-7xl'>TEST TAILWIND</div>
+            <div className='text-center text-yellow-300 text-7xl'>TEST TAILWIND</div>
+            <div className='text-center text-red-300 text-7xl'>TEST TAILWIND</div><div className='text-center text-green-300 text-7xl'>TEST TAILWIND</div>
+            <div className='text-center text-pink-300 text-7xl'>TEST TAILWIND</div><div className='text-center text-green-300 text-7xl'>TEST TAILWIND</div>
+        </>
+    )
+}
+
+export default JustTest
