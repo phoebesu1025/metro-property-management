c17bb967aade86bebb45b78c97ecdb26a3b84e00
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-22 13:38:45 +1300

    tried to see if backend API works for docker compose port:3001✔️

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 console.log("test test")
-
 const express = require('express')
 const app = express()
 app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log("listening to PORT", PORT);
 });
 
-app.get("/", (req, res) => res.json({ message: 'Welcome to Turners API Page!' }))
+app.get("/", (req, res) => res.json({ message: 'Welcome', level: 5, group: 'Geeks On Mission', 'mission No.': 5 }))
 app.get("/risk", (req, res) => res.json({ message: 'Welcome to Risk Rating API!' }))
 app.get("/fifa", (req, res) => {
     res.send("Welcome to FIFA World Cup API!")
