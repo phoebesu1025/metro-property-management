e06fb5a2ae3ad5e69f053ff33d76b2416b61c4f1
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-26 15:31:59 +1300

    sub menu-rentals

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import JustTest from './Components/JustTest'
 import MainMenu from './Components/Header/MainMenu'
+import Rentals from './Components/MegaMenu/Rentals'
 const App = () => {
   return (
     <>
       {/* <JustTest /> */}
       <MainMenu />
+
     </>
   )
 }
