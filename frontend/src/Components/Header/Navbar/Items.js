d5025097dde2d9c54291e467a0bce99c6985e5ee
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-28 19:46:43 +1300

    revised a little bit of the height of the navbar, and fixed position

--- frontend/src/Components/Header/Navbar/Items.js
+++ frontend/src/Components/Header/Navbar/Items.js
@@ -2,13 +2,13 @@ import ArrowButton from "./ArrowButton";
 
 const Items = ({ itemName, itemComponent }) => {
   return (
-    <li className="nav-item flex items-center hover:text-red2 group h-full px-4 ">
+    <li className="nav-item flex items-center hover:text-red2 group h-[3.2rem] px-4 pb-1 ">
       <div className="relative">
         <p className="my-auto"> {itemName} </p>
         <ArrowButton />
       </div>
 
-      <div className="hidden group-hover:block fixed left-0 top-[3.6rem] w-full">
+      <div className="hidden group-hover:block fixed left-0 top-[3.8rem] w-full">
         {itemComponent}
       </div>
 
