57f3d6efe55320542832ee601dc146f848f41f76
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-28 22:26:18 +1300

    import the images files to public folder

--- frontend/src/Components/MegaMenu/RentalsMenu.js
+++ frontend/src/Components/MegaMenu/RentalsMenu.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import LookingIcon from '../images/looking.png'
-import MetroIcon from '../images/service.png'
-import PeopleIcon from '../images/people.png'
-import RecordIcon from '../images/record.png'
-import Arrow from '../images/redArrow.png'
+
 const Rentals = () => {
     return (
         <>
@@ -11,25 +7,25 @@ const Rentals = () => {
             <div className='bg-red2 h-128 flex justify-around text-white py-14 text-paragraph leading-10 px-16 '>
 
                 <div >
-                    <img src={LookingIcon} />
+                    <img src="./images/looking.png" />
                     <h1 className='font-bold mt-4'>Looking to rent</h1>
                     <p> Rental Listings</p>
                     <p> Rental Process</p>
                 </div>
                 <div>
-                    <img src={MetroIcon} />
+                    <img src="./images/service.png" />
                     <h1 className='font-bold mt-4' >Metro Rental Services</h1>
                     <p> Tenant Services</p>
                     <p>Own a home while renting</p>
                     <p>Healthy home standards</p>
                 </div>
                 <div>
-                    <img src={PeopleIcon} />
+                    <img src="./images/people.png" />
                     <h1 className='font-bold mt-4'>Meet a property manager</h1>
                     <p>Book an online meet</p>
                 </div>
                 <div>
-                    <img src={RecordIcon} />
+                    <img src="./images/record.png" />
                     <h1 className='font-bold mt-4'>Keep track of your records</h1>
                     <p className='w-44 leading-5 my-2'>Create an account to keep track of your favorites and rental process.</p>
                     <p className='mb-10 font-bold'>Join My Diary</p>
