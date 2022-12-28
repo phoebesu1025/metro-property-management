d6286346e7b061f07eca16c068ad3688270c1add
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-28 18:48:09 +1300

    separate Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import JustTest from './Components/JustTest'
 import MainMenu from './Components/Header/MainMenu'
-import Footer from './Components/Footer'
+import Footer from './Components/Footer/Footer'
 const App = () => {
   return (
     <div >
-      {/* <JustTest /> */}
       <MainMenu />
       <div><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p><p>hi</p></div>
       <Footer />
---
+++ frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FooterTop from './FooterTop'
+import FooterBottom from './FooterBottom'
+const Footer = () => {
+    return (
+        <div className=''>
+            <FooterTop />
+            <FooterBottom />
+        </div >
+    )
+}
+
+export default Footer
