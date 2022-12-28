cd1ef3d8b15ca392a550dbf68d2b763c0e9ae83a
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-28 18:52:49 +1300

    separate Footer components

---
+++ frontend/src/Components/Header/CompanyLogo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CompanyLogo = () => {
+  return (
+    <div className="company-logo">
+      <img src="./images/logo.png" alt="Metro Logo" />
+    </div>
+  );
+};
+
+export default CompanyLogo;
---
+++ frontend/src/Components/Header/Navbar/ArrowButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ArrowButton = () => {
+  return (
+    <img
+      src="./images/redArrow.png"
+      className=" absolute inset-x-0 mx-auto text-center hidden group-hover:block "
+      alt="Arrow Btn"
+    />
+  );
+};
+
+export default ArrowButton;
