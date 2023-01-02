bcae8cce5e42af96dbd34c206aa541bed6718c14
phoebesu1025 <phoebesu85@yahoo.com.tw>
2023-01-03 02:07:44 +1300

    revised Media Query for RedBanner

---
+++ frontend/src/Components/CommonComponents/RedBanner/RedBanner1/RedBannerButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const RedBannerButton = () => {
+    return (
+        <div className=' my-auto w-full flex flex-col items-center
+        
+        md:flex-none md:w-fit'>
+            <button className='bg-buttonGrey rounded-md shadow-md 
+            px-[1.5rem] py-[1.2rem]  
+
+            xl:px-16 xl:py-3'>
+                Join My Dairy
+            </button>
+        </div>
+    )
+}
+export default RedBannerButton
--- frontend/src/Components/CommonComponents/RedBanner/RedBanner1/RedBannerText.js
+++ frontend/src/Components/CommonComponents/RedBanner/RedBanner1/RedBannerText.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const RedBannerText = () => {
     return (
-        <h1 className='my-auto  text-[2rem]'>
+        <h1 className='my-auto text-[1.5rem]
+        lg:text-[2rem] '>
             Hassle free online tracking process, Join our <span>My Diary</span>
         </h1>
     )
