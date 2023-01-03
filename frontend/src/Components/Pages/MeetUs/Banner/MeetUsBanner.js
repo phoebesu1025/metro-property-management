ca86ef45bc144258e0cfe9693113b4af6a069b7e
Karan Singh <iampatwari@gmail.com>
2023-01-04 09:19:40 +1300

    Upd: meet/filter is responsive now

--- frontend/src/Components/Pages/MeetUs/Banner/MeetUsBanner.js
+++ frontend/src/Components/Pages/MeetUs/Banner/MeetUsBanner.js
@@ -4,13 +4,13 @@ import PropertyManagerSearch from "./Search/PropertyManagerSearch";
 
 const MeetUsBanner = () => {
   return (
-    <div className="meet-us-banner relative mt-40">
+    <div className="meet-us-banner relative mt-40 2xl:mb-20 lg:mb-36 sm:mb-40 xs:mb-60 mb-72">
       <MainBanner
         data={{
           imgSrc: {
             desktop: "meet-us-banner-desktop.png",
-            tablet: "meet-us-banner-desktop.png",
-            mobile: "meet-us-banner-desktop.png ",
+            tablet: "meet-us-banner-tablet.png",
+            mobile: "meet-us-banner-mobile.png ",
           },
           imgAlt: "Meet US banner",
           text: `Stress Free Process During 
--- frontend/src/Components/Pages/MeetUs/Banner/Search/PropertyManagerSearch.js
+++ frontend/src/Components/Pages/MeetUs/Banner/Search/PropertyManagerSearch.js
@@ -1,18 +1,17 @@
-import React from "react";
 import DropdownInputFilter from "./DropdownInputFilter";
 
 const PropertyManagerSearch = () => {
   return (
     <div
-      className={`xl:absolute relative 2xl:px-24 sm:px-12 px-6
-      w-full top-[85%] drop-shadow-2xl
+      className={`absolute 2xl:px-24 sm:px-12 px-6
+      w-full xl:top-[85%] sm:top-[80%]  2xs:top-[70%] top-[88%] drop-shadow-2xl
       `}
     >
       <div
-        className={`flex xl:flex-row flex-wrap gap-2 flex-col justify-between py-8 px-12
-        rounded-3xl bg-white`}
+        className={`flex sm:flex-row flex-wrap gap-2 flex-col justify-between py-8 px-12
+        rounded-xl bg-white`}
       >
-        <div className="name-search flex flex-col gap-y-2">
+        <div className="name-search flex flex-col gap-y-2 2xl:basis-[25%] sm:basis-[45%] basis-[100%]">
           <label>Search by Property manager Name</label>
           <input placeholder="Enter the name" type={"text"} />
         </div>
@@ -31,7 +30,7 @@ const PropertyManagerSearch = () => {
           dropdowns={["Auckland Central", "Albany", "Hamilton"]}
         />
 
-        <div className="flex justify-center items-end">
+        <div className="flex justify-end items-end 2xl:basis-[10%] sm:basis-[45%] basis-[100%]">
           <button className="property-manger-search-btn bg-red1 text-white  px-10 py-2 rounded-md ">
             Search
           </button>
