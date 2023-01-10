d3f93d81533b03d3b551fa2fe729bcfd204fb29c
Phoebe Su <111402381+phoebesu1025@users.noreply.github.com>
2023-01-10 16:42:38 +1300

    Revert "created the send message popUP"

--- frontend/src/Components/Pages/MeetUs/Banner/Search/PopUp/PropertyManagersPopUp1.js
+++ frontend/src/Components/Pages/MeetUs/Banner/Search/PopUp/PropertyManagersPopUp1.js
@@ -1,54 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import PopUpForm1 from './PopUpForm1'
+import React from 'react'
 
 const PropertyManagersPopUp1 = ({ closePopUp }) => {
-    const [showData, setShowData] = useState(true)
-
-    useEffect(() => {
-        fetch("http://localhost:5000/propertyManagers/63b0c46e4aa0047bb15e89a5")
-            .then(response => response.json())
-            .then(result => {
-                console.log(result);
-                setShowData(result);
-            }
-            )
-            .catch(error => console.log('error', error));
-    }, [])
-
     return (
-        <div className=' whole container/position  z-50 absolute top-[50rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-            <div className='box container/ background  bg-white w-[35.5rem] h-[40rem] shadow-2xl'>
-
+        <div className=' whole container/position  z-50 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+            <div className='box container/ background  bg-white w-[35.5rem] h-[36.18rem] shadow-2xl'>
                 <img src="./images/propertyManagers/redLine.png" alt="redline" className='w-[35.5rem]' />
-                <div className=' my-7 mx-10 float-right hover:cursor-pointer' onClick={() => closePopUp(false)} ><img src="./images/propertyManagers/close.png" alt="close" /></div>
+                <div className='flex justify-end my-5 mx-10 hover:cursor-pointer' onClick={() => closePopUp(false)} ><img src="./images/propertyManagers/close.png" alt="close" /></div>
                 {/*  design and close button */}
 
-                <div className='flex mt-7 mb-5 ml-10'>
-                    <div className='my-auto'><img src={showData.imgSrc} alt={showData.imgAlt} className="w-[2.5rem] h-[2.5rem] " /></div>
-                    <div className='flex flex-col justify-center gap-1 ml-4'>
-                        <h1 className='text-[0.75rem] font-semibold'>{showData.name}</h1>
-                        <p className='text-[0.625rem] text-FaqHeading'>{showData.category}</p>
-                    </div>
 
 
 
 
-
-
-
-
-
-
-                </div>
                 <div className='mx-[5.75rem]'>
                     <div><h1 className='text-[1.375rem] font-bold'>Send a message</h1>
                         <p className='text-[0.625rem]'>All fields marked with <span className='text-popUpStar'>*</span> are required()</p>
                     </div>
+                    <form className='mt-[1.5rem]'>
+                        <div className='flex justify-between mb-[1.5rem]'>
+                            <label className='text-footerSubGrey2 text-[0.875rem]'>Name <span className='text-popUpStar'>*</span><br />
+                                <input required type='text' placeholder='Type your name' className='w-[11.375rem] rounded-md  opacity-70 mt-1 placeholder:text-buttonGrey placeholder:text-[0.75rem]' /></label>
+                            <label className='text-footerSubGrey2 text-[0.875rem]'>Property Manager<br />
+                                <input type="text" placeholder='Michael Stuart' className='w-[11.375rem] rounded-md opacity-70 mt-1 placeholder:text-buttonGrey placeholder:text-[0.75rem]' /></label>
+                        </div>
+
+                        <div className='flex justify-between mb-[1.5rem]'>
+                            <label className='text-footerSubGrey2 text-[0.875rem]'>Your Email <span className='text-popUpStar'>*</span><br />
+                                <input required type='email' placeholder='Type your email' className='w-[11.375rem] rounded-md  opacity-70 mt-1 placeholder:text-buttonGrey placeholder:text-[0.75rem]' /></label>
+                            <label className='text-footerSubGrey2 text-[0.875rem]'>Phone number<br />
+                                <input type="tel" pattern=".{8,}" placeholder='Type your phone number' className='w-[11.375rem] rounded-md opacity-70 mt-1 placeholder:text-buttonGrey placeholder:text-[0.75rem] ' /></label>
+                        </div>
+
+                        <div>
+                            <label className='text-footerSubGrey2 text-[0.875rem]'> Message <span className='text-popUpStar'>*</span><br />
+                                <textarea className='w-[24.06rem] h-[6.438rem] rounded-md opacity-70 mt-1' required />
+                            </label>
+                        </div>
+
+                        <div className='flex mt-[2rem]'><button className='bg-red1 rounded-md  text-white text-[0.93rem] font-semibold w-[10.93rem] h-[2.93rem] mx-auto'>Send</button>
+                        </div>
 
 
 
-
-                    <PopUpForm1 />
+                    </form>
                 </div>
 
 
