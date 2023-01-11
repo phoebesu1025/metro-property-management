3897d96b9ed7d01d2e53cf815ba94234f052b2ab
Karan Singh <70357137+imkaransandhu@users.noreply.github.com>
2023-01-11 20:16:39 +1300

    Upd: Dropdown and checkbox

--- backend/Controllers/SearchProperty.js
+++ backend/Controllers/SearchProperty.js
@@ -2,7 +2,23 @@ const Property = require("../db/Property");
 
 const SearchProperty = async (req, res) => {
   try {
-    if (req.query.region === "All Regions") {
+    console.log(req.body);
+    const findProperty = await Property.find(req.body);
+    if (findProperty.length === 0) {
+      throw new Error("No property matching your filtering");
+    }
+
+    res.send(findProperty);
+  } catch (e) {
+    const newError = e;
+    res.status(409).send(`${newError}`);
+  }
+};
+
+module.exports = { SearchProperty };
+
+/*
+  if (req.query.region === "All Regions") {
       req.query.region = `$exists:${true}`;
     }
 
@@ -16,16 +32,4 @@ const SearchProperty = async (req, res) => {
 
     console.log(customQuery);
 
-    const findProperty = await Property.find(req.body);
-    if (!findProperty) {
-      throw new Error("No property found");
-    }
-
-    res.send(findProperty);
-  } catch (e) {
-    const newError = e;
-    res.status(409).send(`${newError}`);
-  }
-};
-
-module.exports = { SearchProperty };
+*/
