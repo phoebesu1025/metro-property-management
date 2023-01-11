21dea3a6c9d79e8a454bf4f1e2865e2413d4d762
phoebesu1025 <phoebesu85@yahoo.com.tw>
2023-01-11 23:11:39 +1300

    Fix: subscription error message now will show under the input field

--- frontend/src/Components/Newsletter/FormNewsletter.js
+++ frontend/src/Components/Newsletter/FormNewsletter.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 const FormNewsletter = () => {
   const [userInput, setUserInput] = useState('');
+  const [customError, setCustomError] = useState('');
 
   const handleChange = (e) => {
     setUserInput(e.target.value);
@@ -24,17 +25,22 @@ const FormNewsletter = () => {
     };
 
     fetch("http://localhost:5000/Subscribe", requestOptions)
-      .then(res => res.json())
+      .then(res => res.text())
       .then(result => {
         console.log(result, "subscribed")
-        if (result.status === "ok") {
+        if (result === "") {
           alert('Subscribed successfully! 🎉🎉🎉');
+
           setUserInput('');
+          setCustomError('')
         } else {
-          alert('You\'already subscribed')
+          return setCustomError(result)
         }
       })
-      .catch(error => console.log('error', error));
+      .catch(error => {
+        console.log(error, "error")
+
+      });
   }
 
 
@@ -47,23 +53,28 @@ const FormNewsletter = () => {
         Your Email Address
       </p>
       <div className="flex gap-10
-      flex-col  items-end 
+      
       
       xs:flex-row xs:justify-between">
-        <input required
-          type="email"
-          className="border-footerInput rounded-md border-solid border  
+        <div className='flex flex-col'>
+          <input required
+            type="email"
+            className="border-footerInput rounded-md border-solid border  
           w-full h-[2.95rem] px-4 py-3
 
           xs:w-[21rem] 
             "
-          onChange={handleChange}
-          value={userInput}
-        />
+            onChange={handleChange}
+            value={userInput}
+          />
+          {customError && <p className="text-red1 font-semibold">{customError}</p>}
+        </div>
+
         <button className=" bg-red2 text-white rounded-md w-[12.5rem] h-[2.95rem] px-10 py-3 " onSubmit={handleSubmit}>
           Subscribe
         </button>
       </div>
+
     </form >
   );
 };
