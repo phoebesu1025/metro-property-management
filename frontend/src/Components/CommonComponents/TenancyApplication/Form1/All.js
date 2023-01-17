9234d46b36712da313860a1bb742ff1192df7861
phoebesu1025 <phoebesu85@yahoo.com.tw>
2023-01-18 02:07:49 +1300

    Update: Chanelle's page

--- frontend/src/Components/CommonComponents/TenancyApplication/Form1/All.js
+++ frontend/src/Components/CommonComponents/TenancyApplication/Form1/All.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PersonalDetails from './PersonalDetails'
 import Pets from './Pets'
 import Preferences from './Preferences'
 import ProofOfId from './ProofOfId'
 import Utilities from './Utilities'
+import PopUpId from './PopUpId/PopUpId'
+import PopUpPet from './PopUpId/PopUpPet'
 
 const All = () => {
+    const [showPopUpId, setShowPopUpId] = useState("")
+    const [showPopUpPet, setShowPopUpPet] = useState("")
+    const popUpBtnId = () => {
+        setShowPopUpId(<PopUpId passClose={closePopUp} />)
+    }
+    const closePopUp = () => {
+        setShowPopUpId()
+    }
+    const popUpBtnPet = () => {
+        setShowPopUpPet(<PopUpPet passClosePet={closePopUpPet} />)
+    }
+    const closePopUpPet = () => {
+        setShowPopUpPet()
+    }
+
+
     return (
         <div>
             <Preferences />
             <PersonalDetails />
-            <ProofOfId />
-            <Pets />
+            <ProofOfId popUpBtnId={popUpBtnId} />
+            {showPopUpId}
+            <Pets popUpBtnPet={popUpBtnPet} />
+            {showPopUpPet}
             <Utilities />
         </div>
     )
